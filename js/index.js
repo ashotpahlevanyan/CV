e91d8a18d42c2cba57a6ce1fd25e8df6b1f49096
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-06-01 13:52:15 +0400

    cosmetic fixed

--- js/index.js
+++ js/index.js
@@ -1,66 +1,40 @@
 (function() {
 
-'use strict';
-let wSize;
-let upToTop = document.querySelector('.upToTop');
+	'use strict';
+	let wSize;
+	let upToTop = document.querySelector('.upToTop');
 
-
-
-document.onreadystatechange = function() {
-	if(document.readyState === 'interactive') {
-		domContentLoaded();
-	} else if(document.readyState === 'complete') {
+	document.onreadystatechange = function() {
+		if(document.readyState === 'interactive') {
+			domContentLoaded();
+		} else if(document.readyState === 'complete') {
+		}
 	}
-}
-function domContentLoaded() {
-	window.addEventListener('scroll', myFunction);
+	
+	function domContentLoaded() {
+		window.addEventListener('scroll', myFunction);
+		upToTop.addEventListener('click', function(e) {
+			window.scrollTo({
+				top: 0,
+				behavior: "smooth"
+			});
+		});
 
-	function myFunction() {
-		if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-			if(hasClass(upToTop, 'hide')) {
-				upToTop.classList.remove('hide');
-			}
-		} else {
-			if(!hasClass(upToTop, 'hide')) {
-				upToTop.classList.add('hide');
+		function myFunction() {
+			if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+				if(hasClass(upToTop, 'hide')) {
+					upToTop.classList.remove('hide');
+				}
+			} else {
+				if(!hasClass(upToTop, 'hide')) {
+					upToTop.classList.add('hide');
+				}
 			}
 		}
 	}
-}
 
-function hasClass(element, cls) {
-	return (' ' + element.className + '').indexOf(' ' + cls + '') > -1;
-}
-
-
-//upToTop.addEventListener('click', runScroll, false);
-
-// function scrollToTop() {
-// 	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
-// 	element = $('body');
-// 	offset = element.offset();
-// 	offsetTop = offset.top;
-// 	$('html, body').animate({scrollTop: offsetTop}, 1000, 'linear');
-// }
-// 
-
-// function runScroll() {
-//   scrollTo(document.body, 0, 600);
-// }
-// var scrollme;
-// // scrollme = document.querySelector("html, body");
-// // scrollme.addEventListener("click", runScroll, false)
-
-// function scrollTo(element, to, duration) {
-//   if (duration <= 0) return;
-//   var difference = to - element.scrollTop;
-//   var perTick = difference / duration * 10;
-
-//   setTimeout(function() {
-//     element.scrollTop = element.scrollTop + perTick;
-//     if (element.scrollTop == to) return;
-//     scrollTo(element, to, duration - 10);
-//   }, 10);
-// }
+	function hasClass(element, cls) {
+		return (' ' + element.className + '').indexOf(' ' + cls + '') > -1;
+	}
 
 })();
